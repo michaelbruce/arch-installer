6117026747c2b0192573c296d8437239aecf7554
Michael Bruce <michael.bruce@singletracksystems.com>
2016-12-29 19:38:36 +0000

    Barebones installation

--- install.cljs
+++ install.cljs
@@ -25,3 +25,20 @@
 (call "mount" ["/dev/sda1" "/mnt"])
 (call "pacstrap" ["/mnt" "base" "base-devel"])
 ;; TODO detect ram amount from cat /proc/meminfo to dynamically set swap file size
+;; TODO complete swapfile enabling - how do you use genfstab to assign swapfile instead of /mnt/swapfile - is this even an issue? it seems so!
+;; (call "fallocate" ["-l" "1024M" "/mnt/swapfile"])
+;; (call "chmod" ["600" "/mnt/swapfile"])
+(call "arch-chroot" ["/mnt" "pacman" "-S" "gvim" "net-tools" "--noconfirm"])
+(call "arch-chroot" ["/mnt" "sed" "-i" "/^#en_US.UTF-8 /s/^#//" "/etc/locale.gen"])
+(call "arch-chroot" ["/mnt" "sed" "-i" "/^#en_GB.UTF-8 /s/^#//" "/etc/locale.gen"])
+(call "arch-chroot" ["/mnt" "locale-gen"])
+(call "arch-chroot" ["/mnt" "/bin/bash" "-c" "echo 'LANG=en_GB.UTF-8' > /etc/locale.conf"])
+(call "arch-chroot" ["/mnt" "/bin/bash" "-c" "ln -s /usr/share/zoneinfo/Europe/London /etc/localtime"])
+(call "arch-chroot" ["/mnt" "/bin/bash" "-c" "hwclock --systohc --utc"])
+(call "arch-chroot" ["/mnt" "/bin/bash" "-c" "mkinitcpio -p linux"])
+(call "arch-chroot" ["/mnt" "/bin/bash" "-c" "pacman -S grub os-prober --noconfirm"])
+(call "arch-chroot" ["/mnt" "/bin/bash" "-c" "grub-install --recheck /dev/sda"])
+(call "arch-chroot" ["/mnt" "/bin/bash" "-c" "grub-mkconfig -o /boot/grub/grub.cfg"])
+
+
+
