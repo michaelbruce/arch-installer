c92328c76b99db759e9c3e6d12b8274088a072ba
Michael Bruce <michael.bruce@singletracksystems.com>
2016-12-29 18:34:31 +0000

    pacman and base packages install on target

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ### TODO
     - a bootstrap script should be available to wget so that there is 
+    - check whether node/clojurescript-nodejs are already installed (not needed for fresh arch install)
+    - script output should be async so that user gets feedback whilst install is occuring to longer running processes.
--- install.cljs
+++ install.cljs
@@ -6,12 +6,14 @@
 (def exec
   (.-exec (node/require "child_process")))
 
-(defn call [arg]
-  (exec
-    arg
-    (fn [err stdout stderr]
-      (.log js/console stdout)
-      (.log js/console stderr))))
+(defn call [function]
+  (let [process (spawn function (clj->js (first args)))]
+    (.on (.-stdout process) "data" (fn [data] (.log js/console (str data))))
+    (.on (.-stderr process) "data" (fn [data] (.log js/console (str data))))
+    (.on process "close"
+         (fn [code] (.log js/console (str function "finished"))))
+    (.on process "error"
+         (fn [code] (.log js/console (str code))))))
 
 ;; TODO detect if gpt or mbr is needed
 (println "1. Disk setup - for now only mbr")
@@ -19,3 +21,7 @@
            "mklabel msdos "
            "mkpart primary ext4 1M 100% "
            "set 1 boot on"))
+(call "mkfs.ext4" ["/dev/sda1"])
+(call "mount" ["/dev/sda1" "/mnt"])
+(call "pacstrap" ["/mnt" "base" "base-devel"])
+;; TODO detect ram amount from cat /proc/meminfo to dynamically set swap file size
