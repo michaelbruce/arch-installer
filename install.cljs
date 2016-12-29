bf5103f33e881709aa9a9d41259cd954e2049aa3
Michael Bruce <michael.bruce@singletracksystems.com>
2016-12-29 15:23:11 +0000

    Scripted mbr parted call

--- install.cljs
+++ install.cljs
@@ -6,7 +6,16 @@
 (def exec
   (.-exec (node/require "child_process")))
 
-(exec "ls"
-      (fn [err stdout stderr]
-        (.log js/console stdout)
-        (.log js/console stderr)))
+(defn call [arg]
+  (exec
+    arg
+    (fn [err stdout stderr]
+      (.log js/console stdout)
+      (.log js/console stderr))))
+
+;; TODO detect if gpt or mbr is needed
+(println "1. Disk setup - for now only mbr")
+(call (str "parted --script /dev/sda "
+           "mklabel msdos "
+           "mkpart primary ext4 1M 100% "
+           "set 1 boot on"))
